5201dc023c79a680ef8b1d4ec3223489c35252e0
ayoajetunmobi <67721103+ayoajetunmobi@users.noreply.github.com>
2021-07-31 13:00:58 +0100

    static root changes

--- purchased/settings.py
+++ purchased/settings.py
@@ -139,8 +139,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS= [os.path.join(BASE_DIR,'static')]
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATICFILES_DIRS= [os.path.join(BASE_DIR,'static_env')]
+STATIC_ROOT = os.path.join(BASE_DIR,'static_root')
 
 MEDIA_URL= '/media/'
 MEDIA_ROOT= os.path.join(BASE_DIR , 'media')
