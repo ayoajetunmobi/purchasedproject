1b5daa92d53bb6bb81908752f44037bcb7bc3a93
ayoajetunmobi <67721103+ayoajetunmobi@users.noreply.github.com>
2021-07-31 13:53:32 +0100

    static files

--- purchased/urls.py
+++ purchased/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('homepage.urls')),
     path('login/',include('login.urls'))
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
 + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
