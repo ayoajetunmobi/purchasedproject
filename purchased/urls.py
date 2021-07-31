c7658124d96fe376de0d9e9df8569f7eb7de4388
ayoajetunmobi <67721103+ayoajetunmobi@users.noreply.github.com>
2021-07-31 13:54:55 +0100

    static and media

--- purchased/urls.py
+++ purchased/urls.py
@@ -23,4 +23,4 @@ urlpatterns = [
     path('',include('homepage.urls')),
     path('login/',include('login.urls'))
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
-+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
