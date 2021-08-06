dab58c2cbcfc7a906cc0fd9f73d32e51caa1988a
ayo ajetunmobi <ayoajetunmobi78@gmail.com>
2021-08-06 23:01:02 +0100

    settings

--- purchased/settings.py
+++ purchased/settings.py
@@ -21,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = '^jqk@4l22)qoni&f--&k6(jjnvz#@_83w75tk7ahq=+o249y*y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = ['localhost','www.shopatpurchased.com','shopatpurchased.com']
+
 
 # Application definition
 
