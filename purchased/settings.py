3691b85624052b0ad0dfee0695d4b6456f06b4eb
ayo ajetunmobi <ayoajetunmobi78@gmail.com>
2022-07-30 20:42:23 +0100

    commit

--- purchased/settings.py
+++ purchased/settings.py
@@ -25,7 +25,7 @@ env = environ.Env()
 environ.Env.read_env()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['wwww.shopatpurchased.com','shopatpurchased.com']
 ALLOWED_HOSTS +=  [ 'wwww.shopatpurchased.com','shopatpurchased.com']
