7a77f90f635780b54e9dd0375b79d1d87b046128
ayo ajetunmobi <ayoajetunmobi78@gmail.com>
2022-12-15 15:14:01 +0100

    commit   celery

--- purchased/celery.py
+++ purchased/celery.py
@@ -24,7 +24,7 @@ app.config_from_object(settings, namespace='CELERY')
 app.conf.beat_schedule = {
     'Send_mail_to_Client': {
         'task': 'homepage.tasks.send_mail_task',
-        'schedule': crontab(hour=2, minute=57, day_of_week=1), #every 30 seconds it will be called
+        'schedule': crontab(), #every 30 seconds it will be called
         #'args': (2,) you can pass arguments also if rquired 
     }
 }
