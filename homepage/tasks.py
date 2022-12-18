b7b0daddfe52d30b31855c89cace49cf60e29faf
ayo ajetunmobi <ayoajetunmobi78@gmail.com>
2022-12-18 10:42:58 +0100

    task.py sendmass mail

--- homepage/tasks.py
+++ homepage/tasks.py
@@ -1,6 +1,6 @@
 from celery import shared_task
 from django.conf import settings
-from django.core.mail import send_mail
+from django.core.mail import send_mass_mail
 from django.contrib.sites.shortcuts import get_current_site
 from django.contrib.auth import get_user_model
 
@@ -9,10 +9,11 @@ User = get_user_model()
 
 @shared_task
 def send_mail_task():
-    my_url  = 'http://shopatpurchased.com'
+    my_url  = 'https://shopatpurchased.com'
     subject = 'Amazing Ember Offers'
     message = 'BUSINESS DEALS FOR EVERY LASU STUDENT \n BUY AND SELL AT EASE , Huray !!! \n' + my_url
     email_from = settings.EMAIL_HOST_USER
     recipient_list = list(User.objects.values_list('email',flat=True))
-    send_mail( subject, message, email_from, recipient_list )
+    mytupple =( subject, message, email_from, recipient_list )
+    send_mass_mail((mytupple), fail_silently= False)
     return "Mail has been sent........"
