eba48412f8d80d5fa5c29a554b66bc57c6d862d7
ayo ajetunmobi <ayoajetunmobi78@gmail.com>
2022-12-20 03:32:48 +0100

    commit

--- homepage/tasks.py
+++ homepage/tasks.py
@@ -3,6 +3,8 @@ from django.conf import settings
 from django.core.mail import send_mass_mail
 from django.contrib.sites.shortcuts import get_current_site
 from django.contrib.auth import get_user_model
+from django.core.mail import EmailMultiAlternatives
+
 
 User = get_user_model()
 
@@ -10,10 +12,10 @@ User = get_user_model()
 @shared_task
 def send_mail_task():
     my_url  = 'https://shopatpurchased.com'
-    subject = 'Visit your profile'
-    message = 'making life around campus easy \n shopatpurchased !!! \n'
+    subject = 'make a post'
+    message =  "Hey there, if you've got a product to sell HIT UP NOW \n shopatpurchased !!! \n"
     email_from = settings.EMAIL_HOST_USER
-    recipient_list = list(User.objects.values_list('email',flat=True))
-    mytupple =( subject, message, email_from, recipient_list )
-    send_mass_mail((mytupple,), fail_silently= False)
+    recipient_ = list(User.objects.values_list('email',flat=True))
+    msg = EmailMultiAlternatives(subject, message, email_from,  bcc=recipient_)
+    msg.send()
     return "Mail has been sent........"
